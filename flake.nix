c4df9bcc1df5de6ad7b855d2c892a833ebc897b2
David Guibert <david.guibert@gmail.com>
2020-09-21 16:57:52 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,16 @@
-
 {
   description = "A flake for building NXSession";
 
-  edition = 201909;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     let
-
-      officialRelease = false;
-
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
-
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-
       # Memoize nixpkgs for different platforms for efficiency.
-      nixpkgsFor = forAllSystems (system:
-        import nixpkgs {
+      nixpkgsFor = system: import nixpkgs {
           inherit system;
           overlays = [ self.overlay ];
-        }
-      );
+      };
     in {
-
       overlay = final: prev: with final; {
         nxsession = substituteAll {
           dir = "bin";
@@ -33,12 +22,11 @@
           src = ./nxsession;
         };
       };
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgsFor system; in
+      rec {
+        legacyPackages = pkgs;
 
-      packages = forAllSystems (system: {
-        inherit (nixpkgsFor.${system}) nxsession;
-      });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.nxsession);
-
-  };
+        defaultPackage = pkgs.nxsession;
+  }));
 }
